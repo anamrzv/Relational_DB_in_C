cbacfcd652be4fbdf1c5a5d52aecec4c9aa897d7
Ana <ana.mrzv@mail.ru>
2022-10-24 22:06:29 +0300

    change table structure, add resultset

--- include/table.h
+++ include/table.h
@@ -14,27 +14,17 @@ struct table_schema {
     struct column* columns[INITIAL_ARRAY_SIZE];
 };
 
-//в свою очередь у контента есть ключ по которому 
-struct row_content {
-    char* key;
-    char* content_pointer; //указателей на опр. ячейку
-};
-
-//у каждой строки должен быть хэш по которому достаем контент
-struct row {
-    char* key;
-    struct row_content* content_start;
-};
-
 struct table {
     struct table_schema* table_schema;
     char name[MAX_NAME_LEN];
-    struct row* rows[INITIAL_ARRAY_SIZE]; //массив записей
+    char* rows_start; //начало записей
+    char* table_start; //начало таблицы в файле
     uint64_t rows_count;
 };
 
 struct resultset {
-    //...
+    uint64_t length;
+    char* cursor; //указатель на начало данных
 };
 
 enum data_type {
